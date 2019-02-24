d7f673be69c2e8355b6f1fad9e13aaad9f0f906f
Michel Pelland <michel.pelland12@gmail.com>
2019-02-24 11:57:35 -0500

    Vanilla JS Version

---
+++ main.js
@@ -0,0 +1,78 @@
+//Chart Options
+const options = {
+  chart: {
+    height: 450,
+    wideth: "100%",
+    type: "bar",
+    background: "#f4f4f4",
+    foreColor: "#333"
+  },
+  series: [
+    {
+      name: "Population",
+      data: [
+        8550405,
+        3971883,
+        2720546,
+        2296224,
+        1567442,
+        1563025,
+        1469845,
+        1394928,
+        1300092,
+        1026908
+      ]
+    }
+  ],
+  xaxis: {
+    categories: [
+      "New York",
+      "Los Angeles",
+      "Cicago",
+      "Houston",
+      "Philadelphia",
+      "Phoenix",
+      "San Antonio",
+      "San Diego",
+      "Dallas",
+      "San Jose"
+    ]
+  },
+  plotOptions: {
+    bar: {
+      horizontal: false
+    }
+  },
+  fill: {
+    colors: ["#f44336"]
+  },
+  dataLabels: {
+    enabled: false
+  },
+  title: {
+    text: "Largest Cities By Population",
+    align: "center",
+    margin: 20,
+    offsetY: 20,
+    style: {
+      fontSize: 20
+    }
+  }
+};
+
+//Init Chart
+const chart = new ApexCharts(document.querySelector("#chart"), options);
+
+//Render Chart
+chart.render();
+
+// Event Method Example
+document.querySelector("button").addEventListener("click", () =>
+  chart.updateOptions({
+    plotOptions: {
+      bar: {
+        horizontal: true
+      }
+    }
+  })
+);
